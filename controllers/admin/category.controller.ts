9adf8cd01deeb0fb07e70e32fd4343617e454d80
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2024-07-19 10:30:17 +0700

    implement filter by status featured

--- controllers/admin/category.controller.ts
+++ controllers/admin/category.controller.ts
@@ -4,13 +4,23 @@ import Category from '../../models/category.model';
 //helpers
 import { getPagination } from "../../helpers/pagination.helper";
 export const index = async (req: Request, res: Response) :Promise<void> =>{
-
+    interface Find {
+        deleted: boolean,
+        status?: string
+    }
+    const find: Find = {
+        deleted: false
+    }
+    const status = req.query.status;
+    if(typeof status === "string" ){
+        find.status = status;
+    }
     try {
-        const count = await Category.countDocuments({deleted: false});
+        const count = await Category.countDocuments(find);
         const pagination = getPagination(req, count, 10);
-        const categories = await Category.find({
-            deleted: false
-        }).limit(pagination.limit).skip(pagination.skip)
+        const categories = await Category.find(find)
+        .limit(pagination.limit)
+        .skip(pagination.skip)
         res.status(200).json({categories, count, pagination})
     } catch (error) {
         res.status(500).json({message: "Lỗi không xác định"});
