5e50ffe58a575674748e2f4105a904d0172c7345
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2024-07-19 11:18:37 +0700

    implement sorting featured and clean code

--- controllers/admin/category.controller.ts
+++ controllers/admin/category.controller.ts
@@ -3,18 +3,22 @@ import Category from '../../models/category.model';
 
 //helpers
 import { getPagination } from "../../helpers/pagination.helper";
-import { buildFindQuery } from "../../helpers/search.helper";
+import { buildFindQuery, buildSorting } from "../../helpers/search.helper";
 export const index = async (req: Request, res: Response) :Promise<void> =>{ 
     
+    const defaultLimit = 10;
     try { 
-
-        const find = buildFindQuery(req);
-        const count = await Category.countDocuments(find);
-        const pagination = getPagination(req, count, 10);
-        const categories = await Category.find(find)
+        //find 
+        const find = buildFindQuery(req); 
+        //sorting 
+        const sort = buildSorting(req); 
+        //total document 
+        const counts = await Category.countDocuments(find);
+        const pagination = getPagination(req, counts, defaultLimit);
+        const categories = await Category.find(find).sort(sort)
         .limit(pagination.limit)
         .skip(pagination.skip)
-        res.status(200).json({categories, count, pagination})
+        res.status(200).json({categories, counts, pagination})
     } catch (error) {
         res.status(500).json({message: "Lỗi không xác định"});
     }
--- helpers/search.helper.ts
+++ helpers/search.helper.ts
@@ -4,13 +4,17 @@ interface Find {
     deleted: boolean,
     status?: string,
     title?: RegExp
+} 
+type sortType = "asc" | "desc"
+interface Sort {
+    [key: string]: sortType
 }
 export const buildFindQuery = (req: Request): Find => {
     const find: Find = { deleted: false };
 
     const keyword = req.query.keyword;
     const status = req.query.status;
-
+    
     if (typeof keyword === 'string') {
         find.title = new RegExp(keyword, "i");
     }
@@ -20,4 +24,14 @@ export const buildFindQuery = (req: Request): Find => {
     }
 
     return find;
+}
+export const buildSorting = (req: Request):Sort =>{ 
+    const sort: Sort = {};
+    const sortKey = req.query.sortKey;
+    const valueKey = req.query.valueKey;
+
+    if(typeof sortKey === 'string' && (valueKey === "asc" || valueKey === "desc")){
+        sort[sortKey] = valueKey
+    }
+    return sort;
 }
