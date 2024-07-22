d59f47a94f13b9be5401ed7e7d48ab1a1901522b
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2024-07-22 10:10:37 +0700

    build suppliers

--- routers/admin/index.router.ts
+++ routers/admin/index.router.ts
@@ -4,6 +4,7 @@ import system from "../../config/system";
 import categoryRouter from '../../routers/admin/category.router';
 import productRouter from '../../routers/admin/product.router';
 import inventoryRouter from '../../routers/admin/inventory.router';
+import supplierRouter from "../../routers/admin/supplier.router";
 //end router
 const {prefixAdmin} = system;
 export default (app: Express): void =>{
@@ -11,5 +12,6 @@ export default (app: Express): void =>{
     const path = `/${prefixAdmin}`;
     app.use(`${path}/categories`,categoryRouter);
     app.use(`${path}/products`,productRouter);
-    app.use(`${path}/inventories`,inventoryRouter)
+    app.use(`${path}/inventories`,inventoryRouter);
+    app.use(`${path}/suppliers`,supplierRouter);
 }
