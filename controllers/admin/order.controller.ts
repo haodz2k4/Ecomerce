c3fdbe350d43db3d562427a35faa960bf0741ec6
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2024-07-30 17:31:37 +0700

    build order detail

--- controllers/admin/order.controller.ts
+++ controllers/admin/order.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import Order from "../../models/order.model";
+import OrderItem from "../../models/order-detail.model";
 import { buildFindQuery, buildSorting } from './../../helpers/search.helper';
 import { getPagination } from './../../helpers/pagination.helper';
 import { Error } from "mongoose";
@@ -23,6 +24,37 @@ export const index = async (req: Request, res: Response) :Promise<void> =>{
             return;
         }    
         res.status(200).json({orders})
+    } catch (error) {
+        if(error instanceof Error){
+            res.status(500).json({message: "Lỗi trong truy vấn cơ sỡ dữ liệu", error: error.message});
+        }else{
+            res.status(500).json({message: "Lỗi không xác định"});
+        }
+    }
+} 
+//[GET] "/admin/orders/detail/:id"
+export const detail = async (req: Request, res: Response) :Promise<void> =>{
+    const id = req.params.id;
+
+    try {
+        const order = await Order
+        .findById(id)
+        .populate('user_id','fullName avatar')
+        .populate('address_id','city street district').select("-deleted")
+        if(!order){
+            res.status(404).json({message: "Hóa đơn không tồn tại"});
+            return; 
+        }
+        const orders = await OrderItem.find({order_id: id}).populate({
+            path: 'product_id',
+            select: 'title avatar status price discountPercentage position',
+            match: {
+                deleted: false,
+                status: "active"
+            }
+        })
+
+        res.status(200).json({order,orders})
     } catch (error) {
         if(error instanceof Error){
             res.status(500).json({message: "Lỗi trong truy vấn cơ sỡ dữ liệu", error: error.message});
