a524b0dc9047c3bd537d957db6e117c2c2823ef7
Hồ Sơn Hào <148406980+haodz2k4@users.noreply.github.com>
2024-07-30 15:50:55 +0700

    implement checkout order featured

--- controllers/clients/checkout.controller.ts
+++ controllers/clients/checkout.controller.ts
@@ -5,21 +5,24 @@ import { Error } from "mongoose";
 //[POST] "/checkout/order"
 export const index = async (req: Request, res: Response) :Promise<void> =>{
     const products = req.body.products;
-    const user_id = res.locals.user.id
+    const user_id = res.locals.user.id;
+    const address_id = req.body.address_id;
     try {
-        const order = new Order({user_id});
+
+        const order = new Order({user_id,address_id});
+        await order.save();
         for(const item of products){
             const orderDetail = new OrderDetail({
                 order_id: order.id,
                 product_id: item.product_id,
-                quantity: item.quantity
+                quantity: item.quantity,
             });
             await orderDetail.save();
         }
-        await order.save();
 
         res.status(200).json({message: "Thêm mới đơn hàng thành công", order})
     } catch (error) {
+        console.error(error)
         if(error instanceof Error){
             res.status(500).json({message: "Lỗi khi thêm vào cơ sỡ dữ liệu", error: error.message})
         }else{
--- routers/clients/checkout.router.ts
+++ routers/clients/checkout.router.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 const router: Router = Router();
 import * as controller from "../../controllers/clients/checkout.controller";
-router.post("/",controller.index);
+router.post("/order",controller.index);
 
 
 export default router;
